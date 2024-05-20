6c2b59a65b438e1fb33b0a2a319f26fdc6c2991f
jhon65496 <jhon65496@yandex.ru>
2024-05-20 19:00:13 +0300

    Add `SolutionService.cs` | Add `ReadSolutionFile()`

---
+++ AccessProjectsRoslynImproveandrepeatComConsoleNet7/Service/SolutionService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessProjectsRoslynImproveandrepeatComConsoleNet7.Service
+{
+    internal class SolutionService
+    {
+        public void ReadSolutionFile()
+        { 
+
+
+        }
+
+    }
+}
